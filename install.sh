7ffce2c463f370f519381c3b6ce55238818b8d2b
mike dupont <mike.dupont@introspector.local>
2024-03-12 11:52:06 -0400

    install

---
+++ install.sh
@@ -0,0 +1,1 @@
+ "sudo apt install -y datefudge docbook docbook-to-man fonts-lmodern gtk-doc-tools  guile-3.0-dev libcmocka-dev libcmocka0 libopts25 libopts25-dev libosp5  libptexenc1 libsofthsm2 libteckit0 libtexlua53 libtexluajit2  libunbound-dev libzzip-0-13 net-tools opensp softhsm2 softhsm2-common  texlive-base texlive-binaries texlive-latex-base texlive-plain-generic"
